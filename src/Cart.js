7a1cd3121db3ee0acab78abbb1b78014760e684b
Shivkumar54 <mshivkumar59@gmail.com>
2023-08-15 17:58:48 +0530

    cart and login

--- src/Cart.js
+++ src/Cart.js
@@ -2,16 +2,15 @@ import React from "react"
 
 const Cart = () => {
   return (
-    <div>
-      <h1>This is cart page </h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo
-        neque nulla obcaecati aliquam. Quam harum ab mollitia velit quasi error
-        eligendi eum accusantium, veniam delectus, doloremque facilis
-        voluptatibus qui dolorem, quo magnam in libero. Labore ipsum cupiditate
-        explicabo ex eligendi, suscipit culpa! Pariatur repellat accusantium qui
-        corporis quas autem nihil.
-      </p>
+    <div className="cartLayout">
+      <div className="cartSection">
+        <img
+          src="https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-4150967-3437888.png"
+          alt="Empty cart"
+          className="emptycart"
+        />
+        <span className="noitemTxt">looks like Cart is empty</span>
+      </div>
     </div>
   )
 }
--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,120 @@
-import React from "react"
-
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { FcGoogle } from "react-icons/fc"
+import { RiFacebookCircleFill, RiMicrosoftFill } from "react-icons/ri"
+import { PiEyeClosedBold, PiEyeBold } from "react-icons/pi"
+import LoginImage from "../Images/animate.json"
+import Lottie from "lottie-react"
 const Login = () => {
+  const navigate = useNavigate()
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [txt, setTxt] = useState(false)
+
+  // form validations
+  const nameRegex = /^[A-Za-z ]{4,}$/
+  const emailRegex = /^[A-Za-z0-9.%_+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/
+  const passRegex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.*[0-9]).{8,}$/
+
+  const validateName = nameRegex.test(name)
+  const validateEmail = emailRegex.test(email)
+  const validatePassword = passRegex.test(password)
+
+  const handleName = (e) => {
+    const usrval = e.target.value
+    setName(usrval)
+  }
+  const handleEmail = (e) => {
+    const usrval = e.target.value
+    setEmail(usrval)
+  }
+  const handlePassword = (e) => {
+    const usrval = e.target.value
+    setPassword(usrval)
+  }
+
+  const SubmitForm = (e) => {
+    e.preventDefault()
+    if (validateName && validateEmail && validatePassword) {
+      console.log("Sucess")
+      setName("")
+      setEmail("")
+      setPassword("")
+      navigate("/")
+    }
+  }
+
   return (
-    <div>
-      <h1>This is Login Page</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit quia
-        obcaecati nesciunt doloribus dolores consequuntur, similique eum
-        architecto aspernatur iusto quae ipsum optio sunt maiores illo cumque
-        quidem minima quas ipsam omnis, corporis amet veniam. Delectus animi,
-        perspiciatis eaque eveniet officia amet, blanditiis soluta magni et ad
-        saepe molestiae laboriosam!
-      </p>
+    <div className="loginLayout">
+      <div className="logincontainer">
+        <div className="namelayout">
+          {/* <img src={LoginImage} alt="LoginImage" className="formImg" /> */}
+          <Lottie animationData={LoginImage} className="formImg" />
+        </div>
+        <div className="vericalline"></div>
+        <div className="formlayout">
+          <form action="" onSubmit={SubmitForm}>
+            <h4 className="formheading">ShopEasy </h4>
+            <p className="formPara">Simple to Shop & Easy to Use</p>
+            <input
+              className="formInput"
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={handleName}
+            />
+            <h5 className="Validator">
+              {name.length > 1 && !validateName
+                ? "* Please enter valid name "
+                : ""}
+            </h5>
+            <input
+              className="formInput"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmail}
+            />
+            <h5 className="Validator">
+              {email.length > 1 && !validateEmail
+                ? "* Please enter valid email "
+                : ""}
+            </h5>
+
+            <input
+              className="formInput passwords"
+              type={txt ? "text" : "password"}
+              placeholder="Password"
+              value={password}
+              onChange={handlePassword}
+            />
+            <span className="passicons">
+              {txt ? (
+                <PiEyeBold onClick={() => setTxt(!txt)} size={20} />
+              ) : (
+                <PiEyeClosedBold onClick={() => setTxt(!txt)} size={20} />
+              )}
+            </span>
+            <h5 className="Validator">
+              {password.length > 1 && !validatePassword
+                ? "* Password should contain [A-Z,0-9, !@#$%^&*]"
+                : ""}
+            </h5>
+
+            <button className="fornbtn">Submit</button>
+            <hr />
+            <h5 className="formsmallheading"> Login using other methods</h5>
+            <div className="iconss">
+              <FcGoogle className="icons" size={30} />
+              <RiFacebookCircleFill className="icons" size={30} />
+              <RiMicrosoftFill className="icons" size={30} />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
